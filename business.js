d9c4c240f16de013e3c176a3e801f3c975b51a0b
60302700 <60302700@udst.edu.qa>
2026-06-25 22:32:58 +0300

    merge conflict

---
+++ business.js
@@ -0,0 +1,9 @@
+import { Login } from "./persistance.js";
+
+async function Login(username, password) {
+    return await Login(username, password);
+}
+
+module.exports = {
+    Login
+};
---
+++ presentation.js
@@ -0,0 +1,10 @@
+import { Login } from "./business.js";
+
+
+async function Login(username, password) {
+    return await Login(username, password);
+}
+
+module.exports = {
+    Login
+};
